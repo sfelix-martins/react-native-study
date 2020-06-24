b440c0acdd301c385ea47f979dc47791a529cfab
Samuel Martins <sfelix_martins@hotmail.com>
2020-06-23 23:34:31 -0300

    :construction: Add initial Sign In form strucuture

--- src/modules/User/pages/SignIn/index.tsx
+++ src/modules/User/pages/SignIn/index.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native-paper';
+import React, { useRef } from 'react';
+import { View, StyleSheet, TextInput as TextInputProps } from 'react-native';
+import { useNavigation, useTheme } from '@react-navigation/native';
+import { Button, TextInput } from 'react-native-paper';
 
 const SignIn: React.FC = () => {
+  const { colors } = useTheme();
+  const passwordRef = useRef<TextInputProps>(null);
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.fill}
+        keyboardAppearance="dark"
+        returnKeyType="next"
+        autoCorrect={false}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        mode="outlined"
+        accessibilityStates
+        label="E-mail"
+        onSubmitEditing={() => passwordRef.current?.focus()}
+      />
+      <TextInput
+        style={styles.fill}
+        ref={passwordRef}
+        keyboardAppearance="dark"
+        returnKeyType="send"
+        secureTextEntry={true}
+        mode="outlined"
+        accessibilityStates
+        label="Password"
+      />
       <Button
-        icon="camera"
-        accessibilityStates="Register"
+        style={styles.fill}
+        mode="contained"
+        accessibilityStates="Login"
         onPress={() => navigation.navigate('SignUp')}>
+        Login
+      </Button>
+      <Button accessibilityStates onPress={() => console.log('Pressed')}>
+        Recovery password
+      </Button>
+      <Button accessibilityStates onPress={() => console.log('Pressed')}>
         Register
       </Button>
     </View>
@@ -21,6 +52,13 @@ const SignIn: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  fill: {
+    width: '100%',
   },
 });
 
