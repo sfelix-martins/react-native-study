b0f8a92421466a6693e844050fedca22b92d1586
Samuel Martins <sfelix_martins@hotmail.com>
2020-07-03 06:52:16 -0300

    :construction: Switch formik to unform on step three

--- src/pages/SignUp/StepThree/index.tsx
+++ src/pages/SignUp/StepThree/index.tsx
@@ -1,18 +1,14 @@
-import React, { useRef } from 'react';
-import { View, StyleSheet, TextInput as TextInputProps } from 'react-native';
-import { TextInput, HelperText, Switch, Text } from 'react-native-paper';
+import React, { useRef, useCallback } from 'react';
+import { StyleSheet, TextInput as TextInputProps } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
 import * as Yup from 'yup';
 
-import {
-  StepperContainer,
-  FormContainer,
-  ContainedButton,
-} from '../../../components/Forms';
-import { Formik } from 'formik';
+import { StepperContainer, Input } from '../../../components/Forms';
 import { useSignUpStepper } from '../../../contexts/signup-steps';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 interface FormValues {
   contactLink?: string;
@@ -24,119 +20,100 @@ interface FormValues {
 const FormSchema = Yup.object().shape({
   contactLink: Yup.string().url('The contact link must to be a valid url'),
   company: Yup.string().max(50, 'The company must have until 10 characters'),
+  phone: Yup.string().min(10),
 });
 
 const StepThree: React.FC = () => {
   const { goToNextStep } = useSignUpStepper();
+  const formRef = useRef<FormHandles>(null);
   const companyRef = useRef<TextInputProps>(null);
   const phoneRef = useRef<TextInputProps>(null);
 
   const navigation = useNavigation();
 
-  const intialValues: FormValues = {
-    contactLink: '',
-    company: '',
-    phone: '',
-    isCertified: false,
-  };
-
   function nextStep(data: FormValues) {
+    console.log('Data', data);
     goToNextStep(data);
     // navigation.navigate('StepFour');
   }
 
+  const validateField = useCallback(async (field: string, value: string) => {
+    try {
+      await Yup.reach(FormSchema, field).validate(value);
+
+      formRef.current?.setErrors({
+        ...formRef.current.getErrors(),
+        [field]: undefined,
+      });
+    } catch (err) {
+      const error = err as Yup.ValidationError;
+
+      formRef.current?.setFieldError(field, error.message);
+    }
+  }, []);
+
+  const handleBlur = useCallback(
+    (field: string) => {
+      validateField(field, formRef.current?.getFieldValue(field));
+    },
+    [validateField],
+  );
+
+  const handleChangeText = useCallback(
+    (field: string, value: string) => {
+      validateField(field, value);
+    },
+    [validateField],
+  );
+
   return (
-    <StepperContainer>
-      <FormContainer>
-        <Formik
-          initialValues={intialValues}
-          validationSchema={FormSchema}
-          onSubmit={(values) => {
-            nextStep(values);
-          }}>
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            setFieldValue,
-            values,
-            errors,
-            touched,
-          }) => (
-            <View style={{ flex: 1, padding: 24 }}>
-              <View style={styles.container}>
-                <TextInput
-                  style={styles.fill}
-                  error={!!errors.contactLink && touched.contactLink}
-                  value={values.contactLink}
-                  onChangeText={handleChange('contactLink')}
-                  onBlur={handleBlur('contactLink')}
-                  returnKeyType="next"
-                  autoCapitalize="none"
-                  mode="outlined"
-                  accessibilityStates
-                  keyboardType="url"
-                  label="Contact link"
-                  onSubmitEditing={() => companyRef.current?.focus()}
-                />
-                {touched.contactLink && errors.contactLink && (
-                  <HelperText type="error">{errors.contactLink}</HelperText>
-                )}
-
-                <TextInput
-                  ref={companyRef}
-                  style={styles.fill}
-                  error={!!errors.company && touched.company}
-                  value={values.company}
-                  onChangeText={handleChange('company')}
-                  onBlur={handleBlur('company')}
-                  returnKeyType="next"
-                  autoCapitalize="none"
-                  mode="outlined"
-                  accessibilityStates
-                  label="Company"
-                  onSubmitEditing={() => phoneRef.current?.focus()}
-                />
-                {touched.company && errors.company && (
-                  <HelperText type="error">{errors.company}</HelperText>
-                )}
-
-                <TextInput
-                  ref={phoneRef}
-                  style={styles.fill}
-                  error={!!errors.phone && touched.phone}
-                  value={values.phone}
-                  onChangeText={handleChange('phone')}
-                  onBlur={handleBlur('phone')}
-                  autoCapitalize="none"
-                  mode="outlined"
-                  accessibilityStates
-                  keyboardType="phone-pad"
-                  label="Phone"
-                />
-                {touched.phone && errors.phone && (
-                  <HelperText type="error">{errors.phone}</HelperText>
-                )}
-
-                <View style={styles.switch}>
-                  <Switch
-                    style={{ marginRight: 16 }}
-                    accessibilityStates
-                    onValueChange={() =>
-                      setFieldValue('isCertified', !values.isCertified)
-                    }
-                    value={values.isCertified}
-                  />
-                  <Text accessibilityStates>I'm a certified analyst</Text>
-                </View>
-              </View>
-              <ContainedButton style={styles.button} onPress={handleSubmit}>
-                Next
-              </ContainedButton>
-            </View>
-          )}
-        </Formik>
-      </FormContainer>
+    <StepperContainer onNext={() => formRef.current?.submitForm()}>
+      <Form style={styles.container} ref={formRef} onSubmit={nextStep}>
+        <Input
+          onBlur={() => handleBlur('contactLink')}
+          onChangeText={(value) => handleChangeText('contactLink', value)}
+          name="contactLink"
+          label="Contact link"
+          style={styles.textInput}
+          returnKeyType="next"
+          autoCorrect={false}
+          mode="outlined"
+          keyboardType="url"
+          autoCapitalize="none"
+          onSubmitEditing={() => companyRef.current?.focus()}
+        />
+        <Input
+          ref={companyRef}
+          onBlur={() => handleBlur('company')}
+          onChangeText={(value) => handleChangeText('company', value)}
+          name="company"
+          label="Company"
+          style={styles.textInput}
+          returnKeyType="next"
+          autoCorrect={false}
+          mode="outlined"
+          onSubmitEditing={() => phoneRef.current?.focus()}
+        />
+        <Input
+          ref={phoneRef}
+          onBlur={() => handleBlur('phone')}
+          onChangeText={(value) => handleChangeText('phone', value)}
+          name="phone"
+          label="Phone"
+          style={styles.textInput}
+          returnKeyType="next"
+          keyboardType="phone-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+          mode="outlined"
+          onSubmitEditing={() => phoneRef.current?.focus()}
+        />
+        {/* <Switch style={{ marginRight: 16 }}
+        accessibilityStates onValueChange=
+        {() => setFieldValue('isCertified', !values.isCertified)}
+        value={values.isCertified}
+        /> */}
+      </Form>
     </StepperContainer>
   );
 };
@@ -151,7 +128,7 @@ const styles = StyleSheet.create({
   logo: {
     marginBottom: 16,
   },
-  fill: {
+  textInput: {
     width: '100%',
   },
   button: {
